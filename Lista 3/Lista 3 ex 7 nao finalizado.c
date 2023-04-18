598768aa6d88eb4780f1f62de34d9c51d1405971
João Cavadinha <93738205+J-cavadinha@users.noreply.github.com>
2023-04-18 20:24:52 -0300

    Add files via upload

---
+++ Lista 3/Lista 3 ex 7 nao finalizado.c
@@ -0,0 +1,22 @@
+//Lista 03 ex 07
+//programa deve ler o valor da despesa a ser paga (real) e a forma de pagamento
+#include<stdio.h>
+float pagamento_dinheiro (float vdespesa)
+{
+	float vdespesa, vpago;
+	if (vpago > vdespesa)
+		{
+			troco = vpago - vdespesa;
+			printf("O troco e de: %.2f", troco);
+		}
+	if (vpago == vdespesa)
+		{
+			printf("Nao ha troco");
+		}
+}
+float main()
+{
+	dec variav
+	printf("Digite a forma de pagamento(D-debito, C-credito e N-dinheiro):");
+	
+}
