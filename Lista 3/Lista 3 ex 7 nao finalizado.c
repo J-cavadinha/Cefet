d8c3b03463ac20527b8b130768f9b67a2dfac564
João Cavadinha <93738205+J-cavadinha@users.noreply.github.com>
2023-05-15 20:04:49 -0300

    Update Lista 3 ex 7.c

--- Lista 3/Lista 3 ex 7 nao finalizado.c
+++ Lista 3/Lista 3 ex 7 nao finalizado.c
@@ -1,30 +1,36 @@
-//Lista 03 ex 07
-//Programa para exibir o troco do cliente dependendo da forma de pagamento
-//D para débito, C para crédito e N para dinheiro
 #include<stdio.h>
-float pagamento_dinheiro (float vdespesa, float vpago)
+float pagamento_dinheiro (float conta)
 {
-	float troco;
-	troco = vpago - vdespesa;
-	return troco;
-}
-void main()
-{
-	float valorconta, valortroco;
-	int mpgto, valorpago;
-	printf("Digite o valor a ser pago:");
-	scanf("%f", &valorconta);
-	printf("Digite o metodo de pagamento:");
-	scanf("%d", &mpgto);
-	
-	if (mpgto == 'D' && mpgto == 'C')
-		printf("Nao ha troco");
-		
-	if (mpgto == 'N')
-	{
-		printf("Valor pago:");
-		scanf("%f", &valorpago);
-		valortroco = pagamento_dinheiro (valorconta, valorpago);
-		printf("O valor do troco e: %.2f", valortroco);
+	float troco, valorpago;
+	printf("Valor pago:");
+	scanf ("%f", &valorpago);
+	troco = conta - valorpago;
+	if (troco > 0)
+		printf("Troco: %.2f", troco);
+	if (troco < 0){
+		troco = troco * -1;
+		printf("%.0f, a mais", troco);
 	}
+	else
+		printf("Sem troco");
+}
+int main()
+{
+	float vconta;
+	char metodopg;
+	printf("Valor a ser pago: ");
+	scanf("%f",&vconta);
+	printf("Digite o metodo de pagamento: ");
+	scanf(" &c ",&metodopg);
+	
+	if (metodopg == 'C' || metodopg == 'D')
+		printf("Sem troco");
+
+	if (metodopg == 'N')
+		pagamento_dinheiro(vconta);
+	
+	else
+		printf("Digito invalido.");
+	
+	return 0;
 }
