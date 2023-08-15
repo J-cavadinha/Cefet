df0db4448a02fd6fd5a1ca7526a54d8122319bf1
João Cavadinha <93738205+J-cavadinha@users.noreply.github.com>
2023-08-14 22:20:52 -0300

    Delete ProjetoAlgoritmoComputacional/Lista 1 directory

--- ProjetoAlgoritmoComputacional/Lista 1/Lista 1 ex 1.c
+++
@@ -1,9 +0,0 @@
-//Função que receba 2 números inteiros, calcule, exiba e retorne a soma deles
-#include<stdio.h>
-int calculaSoma (int num1, int num2)
-{
-	int soma_num;
-	soma_num = num1 + num2
-	printf("Resultado da soma: %d", soma_num);
-	return soma_num;
-}
--- ProjetoAlgoritmoComputacional/Lista 1/Lista 1 ex 2.c
+++
@@ -1,8 +0,0 @@
-//Função que receba 2 notas de um aluno (real), calcule e retorne a média aritm
-#include<stdio.h>
-float calculaMedia (float nota1, float nota2)
-{
-	float media
-	media = (nota1 + nota2);
-	return media;
-}
