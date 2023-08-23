47e8be05377442d154fc4b954ce37834bf1e9308
João Cavadinha <93738205+J-cavadinha@users.noreply.github.com>
2023-08-23 18:18:22 -0300

    Update MergeSort.cpp

--- EstruturaDados/Sorting/MergeSort.cpp
+++ EstruturaDados/Sorting/MergeSort.cpp
@@ -11,31 +11,21 @@ https://www.programiz.com/dsa/merge-sort#google_vignette */
 
 #include<stdio.h>
 
-void MergeSort(int v[], int inicio, int fim){   //Função que realiza as divisões do array
-    if(inicio<fim){
-        int meio = (inicio+fim)/2;
-        MergeSort(v,inicio,meio);               // Função Recursiva - chama ela mesma
-        MergeSort(v,meio+1,fim);                // Função Recursiva
-        
-        MergeSortUne(v,inicio,fim,meio);        // Chama a 2a função que faz a união
-    }
-}
-
-void MergeSortUne(int v, int inicio, int fim, int meio){ // Função que une os arrays
-    int i, aux[8], posl = inicio, inicioA1 = inicio, inicioA2 = meio+1; // Declaração de var
+void Intercala(int v[], int inicio, int fim, int meio){ // Função que une os arrays
+    int i, aux[6], posl = inicio, inicioA1 = inicio, inicioA2 = meio+1; // Declaração de var
     while(inicioA1 <= meio && inicioA2 <= fim){ // inicio menor/igual ao meio e meio+1 menor/igual ao fim
-        if(inicioA1 > inicioA2){    // Se inicio menor que meio +1
+        if(v[inicioA1] > v[inicioA2]){    // Se inicio menor que meio +1
             aux[posl]=v[inicioA2];  // Var auxiliar[inicio] = posição vetor[meio+1]
             inicioA2++;             // Acrescenta 1 pro meio+1
         } else{
-            aux[posl]=A[inicioA1];  // Var auxiliar[posição livre] = vetor[inicio]
+            aux[posl]=v[inicioA1];  // Var auxiliar[posição livre] = vetor[inicio]
             inicioA1++;             // Acrescenta 1 pro inicio
         }
         posl++;                     // Acrescenta 1 pra posição livre
     } //Fim while
 
     for(i=inicioA1; i<=meio;i++){
-        aux[posl]=v[i];
+        aux[posl]=v[i]; 
         posl++;
     }
     for(i=inicioA2; i<=fim; i++){
@@ -45,4 +35,25 @@ void MergeSortUne(int v, int inicio, int fim, int meio){ // Função que une os 
     for(i=inicio; i<=fim; i++){
         v[i]=aux[i];
     }
+}
+
+void MergeSort(int v[], int inicio, int fim){   //Função que realiza as divisões do array
+    int meio;
+    if(inicio<fim){
+        meio = (inicio+fim)/2;
+        MergeSort(v,inicio,meio);               // Função Recursiva - chama ela mesma
+        MergeSort(v,meio+1,fim);                // Função Recursiva
+        Intercala(v,inicio,fim,meio);        // Chama a 2a função que faz a união
+    }
+}
+
+int main(){
+    int v[]={10, 8, 2, 3, 9, 4};
+
+    MergeSort(v,0,5);
+    
+    for (int i = 0; i < 6; i++) {
+        printf("%d ", v[i]);
+    }
+    return 0;
 }
