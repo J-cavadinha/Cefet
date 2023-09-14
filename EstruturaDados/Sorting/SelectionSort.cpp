d197fd0b66548e92c3e25f9786c88b11a8555aa3
João Cavadinha <93738205+J-cavadinha@users.noreply.github.com>
2023-09-14 11:38:28 -0300

    Update SelectionSort.cpp

--- EstruturaDados/Sorting/SelectionSort.cpp
+++ EstruturaDados/Sorting/SelectionSort.cpp
@@ -1,4 +1,4 @@
-/* Selection Sort
+/* Selection Sort - Compplexidade O(nˆ2)
    Consiste em 2 loops 'for', no qual o loop interno faz as comparações, percorrendo o array todo, e após ter realizado
    todo o ciclo (até que j<n), o loop externo realiza as mudanças, troca o primeiro elemento com o menor, e o último
    com o maior.
@@ -8,18 +8,18 @@
 #include<stdio.h>
 void selection(int v[], int n){			
 	int menor, aux;
-	for(int i=0; i<n-1; i++){
+	for(int i=0; i<n-1; i++){ // O(n)
 		menor = i;
-		for(int j=i+1; j<n; j++)
+		for(int j=i+1; j<n; j++) // O(n)
 		{
-			if(v[menor]>v[j])
+			if(v[menor]>v[j]) // O(1)
 			menor = j;
 		}
 		aux = v[i];
 		v[i]=v[menor];
 		v[menor]=aux;
 	}
-}
+} // Complexidade -> O(n) * O(n) = O(nˆ2)
 
 int main(){
 	int n;
