d8a904139eb07a215909237ab606693763c3a161
jcavadinha <joaomcavadinha@gmail.com>
2024-04-01 00:34:11 -0300

    Exercício praticamente finalizado, falta consertar erro que causa a possibilidade de lutas com lutadores de classes diferentes

--- POO/Lista de exercícios/Lista Herança/Ex UFC/PesoPena.java
+++ POO/Lista de exercícios/Lista Herança/Ex UFC/PesoPena.java
@@ -1,2 +1,16 @@
 public class PesoPena extends Lutador {
+    PesoPena(String nome, int idade, double peso) {
+        super(nome, idade, peso);
+    }
+
+    @Override
+    public String impressao(String nome, int idade, double peso) {
+        return nome + "/" + idade + "/" + peso;
+    }
+
+    private int modalidade = 4;
+
+    public int getCategoria() {
+        return this.modalidade;
+    }
 }
--- POO/Lista de exercícios/Lista Herança/Ex UFC/Principal.java
+++ POO/Lista de exercícios/Lista Herança/Ex UFC/Principal.java
@@ -4,22 +4,51 @@ public class Principal {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        Lutador[] lutadores = new Lutador[10];
+        Lutador[] lutadores = new Lutador[7];
 
         for (int i = 0; i < lutadores.length; i++) {
-            System.out.println("Digite o nome do lutador: ");
+            System.out.print("Digite o nome do lutador: ");
             String nome = input.nextLine();
 
-            System.out.println("Digite a idade:");
+            System.out.print("Digite a idade:");
             int idade = input.nextInt();
             input.nextLine();
 
-            System.out.println("Digite o peso: ");
+            System.out.print("Digite o peso: ");
             double peso = input.nextDouble();
             input.nextLine();
 
-            lutadores[i] = new Lutador(nome, idade, peso);
+            if (peso <= 65.0) {
+                lutadores[i] = new PesoPena(nome, idade, peso);
+            } else if (peso > 65.0 && peso <= 83.9) {
+                lutadores[i] = new PesoMedio(nome, idade, peso);
+            } else if (peso > 83.9 && peso <= 93.0) {
+                lutadores[i] = new PesoMeioPesado(nome, idade, peso);
+            } else if (peso > 93.0 && peso <= 120.2) {
+                lutadores[i] = new PesoPesado(nome, idade, peso);
+            } else {
+                System.out.println("Não são aceitos lutadores com mais de 120,2 kg");
+                i--;
+            }
         }
+
+        int numero = input.nextInt();
+        System.out.println(
+                lutadores[numero].impressao(lutadores[numero].getNome(), lutadores[numero].getIdade(),
+                        lutadores[numero].getPeso()));
+
+        Lutador[] novoArrayLutadores = Lutador.possiveisLutas(lutadores, numero);
+        Lutador sorteado = Lutador.sorteioLuta(novoArrayLutadores);
+
+        System.out.println("A luta será: " + lutadores[numero].getNome() + " x " + sorteado.getNome());
+
+        // Código para debug apenas
+        // for (int j = 0; j < lutadores.length; j++) {
+        // System.out.println(
+        // lutadores[j].impressao(lutadores[j].getNome(), lutadores[j].getIdade(),
+        // lutadores[j].getPeso()));
+        // }
+
         input.close();
     }
 }
